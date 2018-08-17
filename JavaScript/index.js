95295202ef8f99c8e645ce2f0b5d9dfda3f1f021
pujanamin007 <42464382+pujanamin007@users.noreply.github.com>
2018-08-17 04:39:17 -0400

    first commit

---
+++ JavaScript/index.js
@@ -0,0 +1,16 @@
+        document.addEventListener("mouseclick", changeText);
+         document.addEventListener("mouseclick", myfun);
+
+
+            function changeText(){
+         	document.getElementById("textchange").innerHTML = 'Lets Go ';
+  
+           
+          
+        function myFunction() {
+    document.getElementById("d1").style.color = "red";
+}
+
+           //document.write(new Date().getFullYear());
+
+          alert("Welcome to my Home Page");
